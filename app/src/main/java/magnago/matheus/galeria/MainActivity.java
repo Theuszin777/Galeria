f096f728163fdd64e5df822c256360228ce4a082
20201tiimi0357 <matheusmagnagoifes@gmail.com>
2022-09-16 11:29:37 -0300

    part1

--- app/src/main/java/magnago/matheus/galeria/MainActivity.java
+++ app/src/main/java/magnago/matheus/galeria/MainActivity.java
@@ -1,9 +1,13 @@
 package magnago.matheus.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,4 +19,46 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
     }
+    
+    public boolean onCreateOptionsMenu (Menu menu) {
+
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+
+    }
+
+    // Definição do que ocorre quando o usuário clicar na TollBar.
+    // Câmera
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.opCamera:
+                dispatchTakePictureIntent();
+                return true;
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+    }
+
+    // Def. Compartilhamento
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void sharePhoto() {
+    }
+
 }
+
--- app/src/main/java/magnago/matheus/galeria/PhotoActivity.java
+++ app/src/main/java/magnago/matheus/galeria/PhotoActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -18,5 +20,15 @@ public class PhotoActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
             actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onCreateOptionsMenu (Menu menu) {
+
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.photo_activity_tb, menu);
+
+        return true;
+
     }
 }
